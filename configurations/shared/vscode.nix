a9da63e5ebf74cc006fc109ea4390eeaf5ea65a3
Daniel Silverstone <dsilvers@digital-scurf.org>
2021-04-21 19:00:17 +0100

    vscode: untested try callPackage override
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- configurations/shared/vscode.nix
+++ configurations/shared/vscode.nix
@@ -1,46 +1,51 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }:
+let
+  package = pkgs.unstable.vscode-with-extensions.override {
+    vscodeExtensions = (
+      with pkgs.unstable.vscode-extensions; [
+        matklad.rust-analyzer
+        ms-python.python
+        ms-vscode.cpptools
+        ms-vscode-remote.remote-ssh
+        ms-vsliveshare.vsliveshare
+        jnoortheen.nix-ide
+        brettm12345.nixfmt-vscode
+        eamodio.gitlens
+        usernamehw.errorlens
+        tamasfe.even-better-toml
+        esbenp.prettier-vscode
+      ]
+    ) ++ pkgs.unstable.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        publisher = "rubymaniac";
+        name = "vscode-direnv";
+        version = "0.0.2";
+        sha256 =
+          "4d5be329d297784c699f2521f9f0cf97e79276543387b96df3149fc35620b4be";
+      }
+      {
+        publisher = "serayuzgur";
+        name = "crates";
+        version = "0.5.9";
+        sha256 =
+          "60721b9e5d91ee5ab02478bca94408982971f4c5a6fb93dcd76bd8c3b7bf4650";
+      }
+      {
+        publisher = "karunamurti";
+        name = "tera";
+        version = "0.0.6";
+        sha256 =
+          "a25976987696ead8a700bf5c0da206369bad6b8b145e52ab4cb894a4b19dd208";
+      }
+
+    ];
+  };
+  my-vscode-package = lib.callPackage package {};
+in
+{
   programs.vscode = {
     enable = true;
-    package = pkgs.unstable.vscode-with-extensions.override {
-      vscodeExtensions = (
-        with pkgs.unstable.vscode-extensions; [
-          matklad.rust-analyzer
-          ms-python.python
-          ms-vscode.cpptools
-          ms-vscode-remote.remote-ssh
-          ms-vsliveshare.vsliveshare
-          jnoortheen.nix-ide
-          brettm12345.nixfmt-vscode
-          eamodio.gitlens
-          usernamehw.errorlens
-          tamasfe.even-better-toml
-          esbenp.prettier-vscode
-        ]
-      ) ++ pkgs.unstable.vscode-utils.extensionsFromVscodeMarketplace [
-        {
-          publisher = "rubymaniac";
-          name = "vscode-direnv";
-          version = "0.0.2";
-          sha256 =
-            "4d5be329d297784c699f2521f9f0cf97e79276543387b96df3149fc35620b4be";
-        }
-        {
-          publisher = "serayuzgur";
-          name = "crates";
-          version = "0.5.9";
-          sha256 =
-            "60721b9e5d91ee5ab02478bca94408982971f4c5a6fb93dcd76bd8c3b7bf4650";
-        }
-        {
-          publisher = "karunamurti";
-          name = "tera";
-          version = "0.0.6";
-          sha256 =
-            "a25976987696ead8a700bf5c0da206369bad6b8b145e52ab4cb894a4b19dd208";
-        }
-
-      ];
-    };
+    package = my-vscode-package;
     userSettings = {
       "update.channel" = "none";
       "rust-analyzer.checkOnSave.command" = "clippy";
