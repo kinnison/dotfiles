8b9e9a82bab5d868effc1b2f324680eb7463b686
Daniel Silverstone <dsilvers@digital-scurf.org>
2021-05-05 23:00:22 +0100

    pkgs: Add initial bitwarden-rofi package
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

---
+++ pkgs/bitwarden-rofi.nix
@@ -0,0 +1,33 @@
+{ stdenv, fetchgit, pkgs, ... }:
+
+stdenv.mkDerivation {
+  name = "bitwarden-rofi";
+  src = fetchgit {
+    url = "https://github.com/mattydebie/bitwarden-rofi.git";
+    rev = "62c95afd5634234bac75855dc705d4da5f4fab69";
+    sha256 = "gv18H+J2pjT6d4qoLTcxUeo4r1xzXhBsOoFFpvl3Deo=";
+  };
+
+  configurePhase = "";
+  buildPhase = "";
+  installPhase = ''
+    mkdir -p $out/bin
+    cp bwmenu lib-bwmenu $out/bin/
+    sed -i -e's|bw |${pkgs.bitwarden-cli}/bin/bw |g' $out/bin/bwmenu
+    sed -i -e's|jq |${pkgs.jq}/bin/jq |g' $out/bin/bwmenu
+    sed -i -e's|rofi |${pkgs.rofi}/bin/rofi |g' $out/bin/bwmenu
+    sed -i -e's|keyctl |${pkgs.keyutils}/bin/keyctl |g' $out/bin/bwmenu
+    sed -i -e's|xclip |${pkgs.xclip}/bin/xclip |g' $out/bin/bwmenu
+    sed -i -e's|notify-send |${pkgs.libnotify}/bin/notify-send |g' $out/bin/bwmenu
+
+    sed -i -e's|jq |${pkgs.jq}/bin/jq |g' $out/bin/lib-bwmenu
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Bitwarden CLI interface using Rofi for menuing";
+    homepage = "https://github.com/mattydebie/bitwarden-rofi/";
+    # maintainers = [];
+    license = licenses.gpl3;
+    platforms = platforms.linux;
+  };
+}
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,10 @@
 with pkgs; {
   # Load our vscode extensions
   vscode = callPackage ./vscode { };
+
+  # Our little scripts
   bins = callPackage ./bins { };
+
+  # Things we've packaged until they're in nixpkgs
+  bitwarden-rofi = callPackage ./bitwarden-rofi.nix { };
 }
