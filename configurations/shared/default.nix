34a00fa5c2ecc1d8446b5e0594ac70422e55987a
Daniel Silverstone <dsilvers@digital-scurf.org>
2022-02-20 11:28:47 +0000

    (minecraft): Enable multimc msa client id
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- configurations/shared/default.nix
+++ configurations/shared/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
-
-{
+let msaClientID = "d73630d5-aac3-4fbf-8697-97bd414c80b1";
+in {
   imports = [
     ./zsh.nix
     ./gui.nix
@@ -17,8 +17,9 @@
   home.packages = with pkgs; [
     # We want Python 3 available
     python3
+    # We enjoy minecraft, so multimc is our best bet for a launcher
+    (multimc.override { inherit msaClientID; })
     # We like to do hardware design, include kicad
     kicad
-    unstable.minecraft
   ];
 }
