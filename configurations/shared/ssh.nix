1168008800d21312ffb88683d75a9ea4df41b039
Daniel Silverstone <dsilvers@digital-scurf.org>
2023-11-21 20:24:17 +0000

    ssh: Ensure authorized_keys is a file not a link

--- configurations/shared/ssh.nix
+++ configurations/shared/ssh.nix
@@ -2,5 +2,15 @@
   home.file.".ssh" = {
     source = "${dotroot}/ssh";
     recursive = true;
+    onChange = ''
+      _setup_sshauthkeys () {
+          echo "Trying to configure SSH authorized_keys..."
+          local confroot
+          confroot="$HOME"/.ssh
+          $DRY_RUN_CMD cat "''${confroot}/authorized_keys.store" > "''${confroot}/authorized_keys"
+      }
+      _setup_sshauthkeys
+      unset _setup_sshauthkeys
+    '';
   };
 }
