f950c819202b72e6806a300b2f411bcad92dd1ef
Daniel Silverstone <dsilvers@digital-scurf.org>
2024-01-02 20:52:49 +0000

    pass: home-manager uses git-sync now
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- configurations/shared/pass.nix
+++ configurations/shared/pass.nix
@@ -5,8 +5,17 @@
     enable = true;
     package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
   };
-  services.password-store-sync.enable = true;
-
+  services.git-sync = {
+    enable = true;
+    repositories = {
+      passStore = {
+        interval = 3600;
+        path = config.programs.password-store.settings.PASSWORD_STORE_DIR;
+        uri =
+          "ssh://gitano@git.gitano.org.uk/personal/dsilvers/password-store.git";
+      };
+    };
+  };
   # Ensure the password store things are in the systemd session
   systemd.user.sessionVariables = config.programs.password-store.settings;
 }
